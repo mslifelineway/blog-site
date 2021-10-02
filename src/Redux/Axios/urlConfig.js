61f5892b65072193d201167f04f42ff067a11eaa
= <=>
2021-10-02 11:30:38 +0530

    server api url configuration done

--- src/Redux/Axios/urlConfig.js
+++ src/Redux/Axios/urlConfig.js
@@ -1,2 +1,5 @@
-export const baseUrl = `http://localhost:4005`;
+export const baseUrl =
+  window.location.hostname === "blog-today.herokuapp.com"
+    ? "https://blog-today.herokuapp.com"
+    : `http://localhost:5000`;
 export const api = `${baseUrl}/api`;
